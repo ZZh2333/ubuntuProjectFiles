373b94f4ba1426d8c4e5218e3fe2d0a3e99ed42d
ZZh2333 <13164108610@163.com>
2021-10-14 15:20:05 +0800

    update leetcode

---
+++ Leetcode/Offer_69_peakIndexInMountainArray.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int i = 0;
+        while (arr[i] < arr[i + 1])
+        {
+            i++;
+        }
+        return i;
+    }
+};
+
+int main()
+{
+    Solution solve;
+    vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(3);
+    arr.push_back(5);
+    arr.push_back(4);
+    arr.push_back(2);
+    cout << solve.peakIndexInMountainArray(arr) << endl;
+    ;
+}

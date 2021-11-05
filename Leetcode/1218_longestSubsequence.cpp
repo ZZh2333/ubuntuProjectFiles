6c039d3b27fcf21c73693a004bcab2175b39647f
ZZh2333 <13164108610@163.com>
2021-11-05 17:04:56 +0800

    update much

---
+++ Leetcode/1218_longestSubsequence.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestSubsequence(vector<int> &arr, int difference)
+    {
+
+    }
+};
+
+int main()
+{
+    Solution solve;
+    vector<int> arr = {1, 3, 5, 7};
+    int n = 1;
+    cout << solve.longestSubsequence(arr, n);
+}

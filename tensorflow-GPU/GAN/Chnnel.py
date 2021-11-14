c757a294fb4c2ea67c80b56fa04ff18ca5970a6e
ZZh2333 <13164108610@163.com>
2021-11-14 18:32:12 +0800

    matplot

--- tensorflow-GPU/GAN/Chnnel.py
+++ tensorflow-GPU/GAN/Chnnel.py
@@ -7,7 +7,8 @@ import pymysql
 
 if __name__ == "__main__":
     try:
-        db = pymysql.connect(host="1.119.44.200",port=30002,user="root",passwd="bdctableau",db="data_portal")
+        print(tf.__version__)
+        # db = pymysql.connect(host="1.119.44.200",port=30002,user="root",passwd="bdctableau",db="data_portal")
         # db = pymysql.connect(host="59.110.230.49",port=3306,user="zouzh",passwd="zouzihan0706",db="class")
     except Exception as e:
         print(e)

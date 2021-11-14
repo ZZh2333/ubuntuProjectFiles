aee152789a84029947d951f18e8ff5d38516bf43
ZZh2333 <13164108610@163.com>
2021-11-14 14:17:13 +0800

    Daily LeetCode

--- tensorflow-GPU/GAN/Chnnel.py
+++ tensorflow-GPU/GAN/Chnnel.py
@@ -1,4 +1,13 @@
 import tensorflow as tf
+import pymysql
 
 # print(tf.cuda.isavailable())
-print(tf.__version__)
+# print(tf.__version__)
+
+
+if __name__ == "__main__":
+    try:
+        db = pymysql.connect(host="1.119.44.200",port=30002,user="root",passwd="bdctableau",db="data_portal")
+        # db = pymysql.connect(host="59.110.230.49",port=3306,user="zouzh",passwd="zouzihan0706",db="class")
+    except Exception as e:
+        print(e)

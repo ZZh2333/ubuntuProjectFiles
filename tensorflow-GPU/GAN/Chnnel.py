ded3d74e040bf6683b3b67a6956eb6fbe5805155
ZZh2333 <13164108610@163.com>
2021-11-25 11:18:50 +0800

    Delete info

--- tensorflow-GPU/GAN/Chnnel.py
+++ tensorflow-GPU/GAN/Chnnel.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import pymysql
 
 # print(tf.cuda.isavailable())
 # print(tf.__version__)
@@ -8,7 +7,5 @@ import pymysql
 if __name__ == "__main__":
     try:
         print(tf.__version__)
-        # db = pymysql.connect(host="1.119.44.200",port=30002,user="root",passwd="bdctableau",db="data_portal")
-        # db = pymysql.connect(host="59.110.230.49",port=3306,user="zouzh",passwd="zouzihan0706",db="class")
     except Exception as e:
         print(e)
